e02f6151ba09f7d488c2ba109c06104b02ccb9f0
hexacry <hexacry@gmail.com>
2022-05-05 23:08:39 -0500

    [Fix] type error

--- window/console.go
+++ window/console.go
@@ -6,92 +6,94 @@ import(
 
 type ConsoleObject Value
 
-func (c ConsoleObject) Assert(condition bool, data ...interface{}) {
-	c.Call("assert", condition, data...)
+func (c ConsoleObject) AsValue() Value { return Value(c) }
+
+func (c ConsoleObject) Assert(data ...interface{}) {
+	c.AsValue().Call("assert", data...)
 }
 
-func (c ConsoleObject) Clear() { c.Call("clear") }
+func (c ConsoleObject) Clear() { c.AsValue().Call("clear") }
 
 func (c ConsoleObject) Count(label ...interface{}) {
 	if len(label) > 0 {
-		c.Call("count", label[0])
+		c.AsValue().Call("count", label[0])
 	} else {
-		c.Call("count")
+		c.AsValue().Call("count")
 	}
 }
 
-func (c ConsoleObject) Error(data ...interface{}) { c.Call("error", data...) }
+func (c ConsoleObject) Error(data ...interface{}) { c.AsValue().Call("error", data...) }
 
 func (c ConsoleObject) Group(label ...interface{}) {
 	if len(label) > 0 {
-		c.Call("group", label[0])
+		c.AsValue().Call("group", label[0])
 	} else {
-		c.Call("group")
+		c.AsValue().Call("group")
 	}
 }
 
 func (c ConsoleObject) GroupCollapsed(label ...interface{}) {
 	if len(label) > 0 {
-		c.Call("groupCollapsed", label[0])
+		c.AsValue().Call("groupCollapsed", label[0])
 	} else {
-		c.Call("groupCollapsed")
+		c.AsValue().Call("groupCollapsed")
 	}
 }
 
 func (c ConsoleObject) GroupEnd(label ...interface{}) {
 	if len(label) > 0 {
-		c.Call("groupEnd", label[0])
+		c.AsValue().Call("groupEnd", label[0])
 	} else {
-		c.Call("groupEnd")
+		c.AsValue().Call("groupEnd")
 	}
 }
 
-func (c ConsoleObject) Info(data ...interface{}) { c.Call("info", data...) }
+func (c ConsoleObject) Info(data ...interface{}) { c.AsValue().Call("info", data...) }
 
-func (c ConsoleObject) Log(data ...interface{}) { c.Call("log", data...) }
+func (c ConsoleObject) Log(data ...interface{}) { c.AsValue().Call("log", data...) }
 
 func (c ConsoleObject) Table(tabularData interface{}, props ...interface{}) {
 	if len(props) > 0 {
-		c.Call("table", tabularData, props[0])
+		c.AsValue().Call("table", tabularData, props[0])
 	} else {
-		c.Call("table", tabularData)
+		c.AsValue().Call("table", tabularData)
 	}
 }
 
 func (c ConsoleObject) Time(label ...interface{}) {
 	if len(label) > 0 {
-		c.Call("time", label[0])
+		c.AsValue().Call("time", label[0])
 	} else {
-		c.Call("time")
+		c.AsValue().Call("time")
 	}
 }
 
 func (c ConsoleObject) TimeLog(label ...interface{}, data ...interface{}) {
 	if len(label) > 0 {
 		if len(data) > 0 {
-			c.Call("timeLog", label[0], data...)
+			c.AsValue().Call("timeLog", label[0], data...)
 		} else {
-			c.Call("timeLog", label[0])
+			c.AsValue().Call("timeLog", label[0])
 		}
 	} else {
-		c.Call("timeLog")
+		c.AsValue().Call("timeLog")
 	}
 }
 
 func (c ConsoleObject) TimeEnd(label ...interface{}) {
 	if len(label) > 0 {
-		c.Call("timeEnd", label[0])
+		c.AsValue().Call("timeEnd", label[0])
 	} else {
-		c.Call("timeEnd")
+		c.AsValue().Call("timeEnd")
 	}
 }
 
 func (c ConsoleObject) Trace(label ...interface{}) {
 	if len(label) > 0 {
-		c.Call("trace", label[0])
+		c.AsValue().Call("trace", label[0])
 	} else {
-		c.Call("trace")
+		c.AsValue().Call("trace")
 	}
 }
 
-func (c ConsoleObject) Warn(data ...interface{}) { c.Call("warn", data...) }
+func (c ConsoleObject) Warn(data ...interface{}) { c.AsValue().Call("warn", data...) }
