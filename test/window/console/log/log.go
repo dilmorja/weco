1fad91f62f060384597f18cab1272a701f474af2
hexacry <hexacry@gmail.com>
2022-05-10 19:58:12 -0500

    [Add] some tests

--- test/window/console/log/log.go
+++ test/window/console/log/log.go
@@ -1,3 +1,4 @@
+// go:build js && wasm
 // +build js,wasm
 
 package main
@@ -5,5 +6,17 @@ package main
 import "github.com/dilmorja/weco/window"
 
 func main() {
-	window.Console.Log("TEST PASSED")
+
+	/// window.Console ///
+	// TEST: Log
+	window.Console.Log("window.Console.Log: TEST PASSED")
+
+	// TEST: Info
+	window.Console.Info("window.Console.Info: TEST PASSED")
+
+	// TEST: Warn
+	window.Console.Warn("window.Console.Warn: TEST PASSED")
+
+	// TEST: Error
+	window.Console.Error("window.Console.Error: TEST PASSED")
 }
